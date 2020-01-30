4197806e2949f79da7200f6a52c9d4bbbd87fc11
NoneNoname <51082447+NoneNoname@users.noreply.github.com>
2020-01-30 17:54:47 +0100

    AfkTimer: setInterval, remove expression

--- BondageClub/Scripts/AfkTimer.js
+++ BondageClub/Scripts/AfkTimer.js
@@ -1,21 +1,37 @@
+"use strict"
 
-var AfkTimerTimeoutTimeMs = 1000 * 60 * 5; //5 minutes
+var AfkTimerIncrementMs = 1000 * 60; // 1 minutes
+var AfkTimerTimout = 5; // AfkTimerIncrementMs * 5  ==> 5 minutes
+var AfkTimerIdle = 0;
+var AfkTimerIsSet = false;
 var AfkTimerIsEnabled = null;
 
 var AfkTimerEventsList = ['mousedown', 'mousemove', 'keypress', 'scroll', 'touchstart'];
-var AfkTimerTimeoutID;
+var AfkTimerID = null;
 
 function AfkTimerReset() {
-    clearTimeout(AfkTimerTimeoutID);
-    AfkTimerTimeoutID = setTimeout(AfkTimerSetIsAfk, AfkTimerTimeoutTimeMs);
+    AfkTimerIdle = 0;
+    if (AfkTimerIsSet) {
+        AfkTimerIsSet = false;
+        CharacterSetFacialExpression(Player, "Emoticon", null);
+    }
+}
+
+function AfkTimerIncrement() {
+    if (++AfkTimerIdle >= AfkTimerTimout) {
+        AfkTimerSetIsAfk();
+    }
 }
 
 function AfkTimerStart() {
     AfkTimerEventsList.forEach(e => document.addEventListener(e, AfkTimerReset, true));
+    AfkTimerID = setInterval(AfkTimerIncrement, AfkTimerIncrementMs);
 }
 
 function AfkTimerStop() {
     AfkTimerEventsList.forEach(e => document.removeEventListener(e, AfkTimerReset, true));
+    if (AfkTimerID != null) clearInterval(AfkTimerID);
+    AfkTimerID = null;
 }
 
 function AfkTimerSetEnabled(Enabled) {
@@ -32,4 +48,5 @@ function AfkTimerSetEnabled(Enabled) {
 function AfkTimerSetIsAfk() {
     if (CurrentScreen != "ChatRoom") return;
     CharacterSetFacialExpression(Player, "Emoticon", "Afk");
+    AfkTimerIsSet = true;
 }
